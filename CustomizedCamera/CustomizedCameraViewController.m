090fc8451c7d7df51747a0b5b661390f8e2f8eda
Ocean Lin <shaochieh.lin@gmail.com>
2014-02-14 11:14:19 +0800

    Add overlay customized UI to the imagePickerController’s cameraOverlayView.

--- CustomizedCamera/CustomizedCameraViewController.m
+++ CustomizedCamera/CustomizedCameraViewController.m
@@ -8,11 +8,16 @@
 
 #import "CustomizedCameraViewController.h"
 
-@interface CustomizedCameraViewController ()
+@interface CustomizedCameraViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) UIImagePickerController *imagePickerController;
+@property (strong, nonatomic) IBOutlet UIView *overlayView;
 
 @end
 
 @implementation CustomizedCameraViewController
+
+#pragma mark - Initialization
 
 - (void)viewDidLoad
 {
@@ -24,6 +29,102 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UIImagePickerController *)imagePickerController
+{
+    if (!_imagePickerController) {
+        _imagePickerController = [[UIImagePickerController alloc] init];
+    }
+    
+    return _imagePickerController;
+}
+
+#pragma mark - Delegate/Event handler
+
+- (IBAction)enableCamera:(id)sender {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        self.imagePickerController.delegate = self;
+        self.imagePickerController.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];
+        self.imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+        
+        [self setCustomizedCameraUI];
+        
+        self.imagePickerController.showsCameraControls = NO;
+        
+        [self presentViewController:self.imagePickerController animated:YES completion:NULL];
+    } else {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Enable camera" message:@"Camera is not available." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alertView show];
+    }
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    self.imageView.image = info[UIImagePickerControllerOriginalImage];
+    [self dismissViewControllerAnimated:YES completion:NULL];
+    self.imagePickerController = nil;
+    if (self.imagePickerController) {
+        NSLog(@"picker instance is still there.");
+    }
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+    self.imagePickerController = nil;
+    if (self.imagePickerController) {
+        NSLog(@"picker instance is still there.");
+    }
+}
+
+//- (IBAction)capturePhoto:(id)sender
+//{
+//    [self.imagePickerController takePicture];
+//}
+
+#pragma mark - OverlayView Delegate/Event handler
+
+- (IBAction)drawContent:(id)sender {
+}
+
+
+- (IBAction)textContent:(id)sender {
+}
+
+
+- (IBAction)importPhoto:(id)sender {
+}
+
+- (IBAction)captureWithSend:(id)sender {
+    [self.imagePickerController takePicture];
+}
+
+- (IBAction)reverseCamera:(id)sender {
+}
+
+- (IBAction)lightingControl:(id)sender {
+}
+
+#pragma mark - Functions
+
+- (void)setCustomizedCameraUI
+{
+//Add a button to the cameraOverlayView directly
+//    UIButton *captureBTN = [[UIButton alloc] init];
+//    [captureBTN setImage:[UIImage imageNamed:@"camera_btn_n"] forState:UIControlStateNormal];
+//    
+//    CGRect captureBTNFrame = CGRectMake(50, 50, 50, 50);
+//    captureBTN.frame = captureBTNFrame;
+//    
+//    [captureBTN addTarget:self action:@selector(capturePhoto:) forControlEvents:UIControlEventTouchUpInside];
+//    
+//    [imagePicker.cameraOverlayView addSubview:captureBTN];
+    
+    [[NSBundle mainBundle] loadNibNamed:@"OverlayView" owner:self options:nil];
+    //self.overlayView.frame = self.imagePickerController.cameraOverlayView.frame;
+    self.imagePickerController.cameraOverlayView = self.overlayView;
+    self.overlayView = nil;
 }
 
 @end
